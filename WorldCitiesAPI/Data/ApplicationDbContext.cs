74e65889253cc1a59e1ce00ca06f5260f2f64ae2
Sandeep Murmu <murmusandeep17@gmail.com>
2023-06-30 22:50:32 +0530

    first commit

---
+++ WorldCitiesAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WorldCitiesAPI.Data.Models;
+
+namespace WorldCitiesAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() : base()
+        {
+        }
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<City> Cities => Set<City>();
+        public DbSet<Country> Countries => Set<Country>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<City>().ToTable("Cities");
+            modelBuilder.Entity<City>()
+            .HasKey(x => x.Id);
+            modelBuilder.Entity<City>()
+            .Property(x => x.Id).IsRequired();
+            modelBuilder.Entity<City>()
+            .Property(x => x.Lat).HasColumnType("decimal(7,4)");
+            modelBuilder.Entity<City>()
+            .Property(x => x.Lon).HasColumnType("decimal(7,4)");
+
+            modelBuilder.Entity<Country>().ToTable("Countries");
+            modelBuilder.Entity<Country>()
+            .HasKey(x => x.Id);
+            modelBuilder.Entity<Country>()
+            .Property(x => x.Id).IsRequired();
+            modelBuilder.Entity<City>()
+            .HasOne(x => x.Country)
+            .WithMany(y => y.Cities)
+            .HasForeignKey(x => x.CountryId);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+    }
+}
